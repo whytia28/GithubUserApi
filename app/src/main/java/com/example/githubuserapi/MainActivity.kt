280dcdb93a0a98631e004e87de6fa0a88dfff02a
whytia28 <wahyu.sp9@gmail.com>
2020-07-01 18:23:50 +0700

    parsing sukses

---
+++ app/src/main/java/com/example/githubuserapi/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.githubuserapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.githubuserapi.adapter.UserAdapter
+import com.example.githubuserapi.model.MainViewModel
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: UserAdapter
+    private lateinit var mainViewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        adapter = UserAdapter()
+        adapter.notifyDataSetChanged()
+
+        recycler_view.layoutManager = LinearLayoutManager(this)
+        recycler_view.adapter = adapter
+
+        mainViewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.NewInstanceFactory()
+        ).get(MainViewModel::class.java)
+
+        btn_search.setOnClickListener {
+            val username = edt_search.text.toString()
+            if (username.isEmpty()) return@setOnClickListener
+            showLoading(true)
+
+            mainViewModel.setUser(username)
+        }
+
+        mainViewModel.getUser().observe(this, Observer { userItems ->
+            if (userItems != null) {
+                adapter.setData(userItems)
+                showLoading(false)
+            }
+        })
+    }
+
+    private fun showLoading(state: Boolean) {
+        if (state) {
+            progress_bar.visibility = View.VISIBLE
+        }else{
+            progress_bar.visibility = View.GONE
+        }
+    }
+}
